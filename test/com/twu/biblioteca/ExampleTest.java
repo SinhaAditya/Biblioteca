58fe16a17e08f1cce6daa109a1061b0a22265564
Aditya Sinha <u6010425@anu.edu.au>
2019-10-02 15:50:06 +0530

    Unit testing methods of class Book

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,34 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ExampleTest {
 
+    Book testBook = new Book("Ramayana", "Valmeeki", "3000 BCE");
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testBookGetTitle() {
+        assertThat(testBook.getTitle(), is(equalTo("Ramayana")));
     }
+
+    @Test
+    public void testBookGetAuthor() {
+        assertThat(testBook.getAuthor(), is(equalTo("Valmeeki")));
+    }
+
+    @Test
+    public void testBookGetYear() {
+        assertThat(testBook.getYear(), is(equalTo("3000 BCE")));
+    }
+
+    @Test
+    public void testBookPrintBook() {
+        //insert test here
+    }
+
 }
